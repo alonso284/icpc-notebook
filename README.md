16c9460c9a130fbfe81d889030d5605214ced2dc
Alonso Huerta <alonso12_284@outlook.com>
2024-05-20 15:17:07 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lancheros de Balzapote (ITESM MTY)
+
+- Sorting and Searching	
+- Dynamic Programming
+- Graph Algorithms
+- Range Queries
+    - Fenwick Tree
+    - Sqrt Decomposition
+    - Segment Tree
+    - Treap (Cartesian tree)
+    - Sqrt Tree
+- Tree Algorithms
+- Mathematics
+- String Algorithms
+- Geometry
+- Game Theory
---
+++ template.cpp
@@ -0,0 +1,53 @@
+// Alonso Huerta Escalante
+// Link 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <numeric>
+#include <functional>
+#include <random>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vpii> vvpii;
+typedef vector<vpll> vvpll;
+typedef vector<string> vs;
+typedef vector<char> vc;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define RFOR(i, a, b) for(int i = a; i >= b; i--)
+#define REP(i, a) for(int i = 0; i < a; i++)
+#define RREP(i, a) for(int i = a-1; i >= 0; i--)
+#define FORIT(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define FORITR(it, a) for(auto it = a.rbegin(); it != a.rend(); it++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+const int MOD = 1e9+7;
+const int INF = INT_MAX;
+
+int main(){
+	IOS;
+	return 0;
+}
