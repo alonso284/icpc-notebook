b4d4eee537b65702553cca3b77052e230aed1bef
Alonso Huerta <alonso12_284@outlook.com>
2024-06-16 10:39:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-problem="$1"
+# Check if the program parameter is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <program>"
+  exit 1
+fi
 
-# Compile the C++ code
-g++ "$problem/$problem.cpp" -o "$problem/$problem.exe"
+program=$1
 
-# Run the executable with input from in*.txt
-for input_file in $problem/*.txt; do
-    echo "Input from $input_file:"
-    echo 
-    cat "$input_file"
-    echo
-    echo "Output:"
-    echo
-    ./"$problem/$problem.exe" < "$input_file"
-    echo
-    echo "------------------------------------------"
+# Compile the program
+g++ "$program/$program.cpp" -o "$program.exe" --std=c++20
+
+# Check if the compilation was successful
+if [ $? -ne 0 ]; then
+  echo "Compilation failed."
+  exit 1
+fi
+
+echo "Compilation successful. Running $program.exe with all .txt files as input."
+
+# Run the program with all .txt files in the current directory as input
+for input_file in *.txt; 
+do
+  if [ -f "$input_file" ]; then
+    echo "Running $program.exe with input file $input_file"
+    ./"$program.exe" < "$input_file"
+  else
+    echo "No .txt files found in the current directory."
+    exit 1
+  fi
 done
-
