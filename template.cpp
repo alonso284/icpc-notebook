6f994afc7081c01b1828240dd23679e32c3d95b8
Alonso Huerta <alonso12_284@outlook.com>
2024-06-16 10:40:16 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -14,6 +14,7 @@
 #include <random>
 #include <cstring>
 #include <climits>
+#include <iomanip>
 
 using namespace std;
 
@@ -22,8 +23,13 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
+typedef vector<vi> MatrixI;
+typedef vector<vll> MatrixLL;
 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define For(i,a,b) for(int i = int(a); i < (int)(b); i++)
+#define ForR(i,a,b) for(int i = int(b) - 1; i >= (int)(a); i--)
+#define ForI(i,a,b) for(int i = int(a); i <= (int)(b); i++)
 
 const int MOD = 1e9+7;
 const int INF = INT_MAX;
